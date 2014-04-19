3a4c7c59dc8f1020378c62c1c9ad95eeac7c3f73
Nickolai Novik <is.infinity@yahoo.com>
2014-04-19 21:27:19 +0300

    copy paste some docs

--- aiobeanstalk/handlers.py
+++ aiobeanstalk/handlers.py
@@ -100,15 +100,11 @@ def _interaction(*responses):
     return decortor
 
 
-@_interaction(OK('OK', ['bytes'], True, yaml_parser))
-def process_stats():
-    # <data> (YAML struct)
-    return 'stats\r\n'
-
-
 @_interaction(OK('INSERTED', ['jid']), Buried('BURIED', ['jid']))
 def process_put(data, pri=1, delay=0, ttr=60):
-    """ """
+    """The "put" command is for any process that wants to insert a job into the queue.
+    It comprises a command line followed by the job body::
+    """
     data_len = len(data)
     if data_len >= MAX_JOB_SIZE:
         msg = 'Job size is {} (max allowed is {}'.format(data_len, MAX_JOB_SIZE)
@@ -119,22 +115,28 @@ def process_put(data, pri=1, delay=0, ttr=60):
 
 @_interaction(OK('USING', ['tube']))
 def process_use(tube):
-    """ """
+    """The "use" command is for producers. Subsequent put commands will put jobs into
+    the tube specified by this command. If no use command has been issued, jobs
+    will be put into the tube named "default"."""
     check_name(tube)
     return 'use {}\r\n'.format(tube)
 
 
 @_interaction(OK('RESERVED', ['jid', 'bytes'], True))
 def process_reserve():
-    """ """
+    """This will return a newly-reserved job. If no job is available to be reserved,
+    beanstalkd will wait to send a response until one becomes available. """
     return 'reserve\r\n'
 
 
 @_interaction(OK('RESERVED', ['jid', 'bytes'], True), TimeOut('TIMED_OUT'))
 def process_reserve_with_timeout(timeout=0):
-    """ 
+    """This will return a newly-reserved job. If no job is available to be reserved,
+    beanstalkd will wait to send a response until one becomes available. Once a
+    job is reserved for the client, the client has limited time to run (TTR) the
+    job before the job times out. When the job times out, the server will put the
+    job back into the ready queue
 
-    :rtype timeout: ``int``
     """
     if int(timeout) < 0:
         raise AttributeError('timeout must be greater than 0')
@@ -143,79 +145,94 @@ def process_reserve_with_timeout(timeout=0):
 
 @_interaction(OK('DELETED'))
 def process_delete(jid):
-    """ """
+    """The delete command removes a job from the server entirely. It is normally used
+    by the client when the job has successfully run to completion. A client can
+    delete jobs that it has reserved, ready jobs, delayed jobs, and jobs that are
+    buried."""
     return 'delete {}\r\n'.format(jid)
 
 
 @_interaction(OK('RELEASED'), Buried('BURIED'))
 def process_release(jid, pri=1, delay=0):
-    """ """
+    """The release command puts a reserved job back into the ready queue (and marks
+    its state as "ready") to be run by any client. It is normally used when the job
+    fails because of a transitory error."""
     return 'release {jid} {pri} {delay}\r\n'.format(**locals())
 
 
 @_interaction(OK('BURIED'))
 def process_bury(jid, pri=1):
-    """ """
+    """The "ignore" command is for consumers. It removes the
+    watch list for the current connection."""
     return 'bury {jid} {pri}\r\n'.format(**locals())
 
 
 @_interaction(OK('WATCHING', ['count']))
 def process_watch(tube):
-    """ """
+    """The "watch" command adds the named tube to the watch list for the current
+    connection. A reserve command will take a job from any of the tubes in the
+    watch list. For each new connection, the watch list initially consists of one
+    tube, named "default"""
     check_name(tube)
     return 'watch {}\r\n'.format(tube)
 
 
 @_interaction(OK('WATCHING', ['count']))
 def process_ignore(tube):
-    """ """
+    """The "ignore" command is for consumers. It removes the named tube from the
+    watch list for the current connection"""
     check_name(tube)
     return 'ignore {}\r\n'.format(tube)
 
 
 @_interaction(OK('FOUND', ['jid', 'bytes'], True))
 def process_peek(jid=0):
-    """
-
-    :type jid: object
-    """
+    """The peek commands let the client inspect a job in the system."""
     if jid:
         return 'peek {}\r\n'.format(jid)
 
 
 @_interaction(OK('FOUND', ['jid', 'bytes'], True))
 def process_peek_ready():
-    """ """
+    """Return the next ready job"""
     return 'peek-ready\r\n'
 
 
 @_interaction(OK('FOUND', ['jid', 'bytes'], True))
 def process_peek_delayed():
-    """ """
+    """Return the delayed job with the shortest delay left"""
     return 'peek-delayed\r\n'
 
 
 @_interaction(OK('FOUND', ['jid', 'bytes'], True))
 def process_peek_buried():
-    """ """
+    """Return the next job in the list of buried jobs."""
     return 'peek-buried\r\n'
 
 
 @_interaction(OK('KICKED', ['count']))
 def process_kick(bound=10):
-    """ """
+    """The kick command applies only to the currently used tube. It moves jobs into
+    the ready queue. If there are any buried jobs, it will only kick buried jobs.
+    Otherwise it will kick delayed jobs."""
     return 'kick {}\r\n'.format(bound)
 
 
 @_interaction(OK('TOUCHED'))
 def process_touch(jid):
-    """ """
+    """The "touch" command allows a worker to request more time to work on a job.
+    This is useful for jobs that potentially take a long time, but you still want
+    the benefits of a TTR pulling a job away from an unresponsive worker. A worker
+    may periodically tell the server that it's still alive and processing a job
+    (e.g. it may do this on DEADLINE_SOON). The command postpones the auto
+    release of a reserved job until TTR seconds from when the command is issued."""
     return 'touch {}\r\n'.format(jid)
 
 
 @_interaction(OK('OK', ['bytes'], True, yaml_parser))
 def process_stats():
-    """ """
+    """The stats command gives statistical information about the
+    system as a whole."""
     return 'stats\r\n'
 
 
